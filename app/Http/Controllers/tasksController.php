27490e3bd1f455b93bd9dd7b562529055c84c5df
EC2 Default User <ec2-user@ip-172-31-9-150.us-east-2.compute.internal>
2018-08-28 16:09:42 +0000

    kadai tasklist

--- app/Http/Controllers/tasksController.php
+++ app/Http/Controllers/tasksController.php
@@ -44,8 +44,15 @@ class tasksController extends Controller
      */
     public function store(Request $request)
     {
+        
+        $this->validate($request, [
+            'content' => 'required|max:191',
+            'status' => 'required|max:10',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         return redirect('/');
@@ -90,8 +97,14 @@ class tasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+            'status' => 'required|max:10',
+        ]);
+        
         $task = Task::find($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
 
         return redirect('/');
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,8 +3,14 @@
 @section('content')
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
+    
+   
+
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    
+    {!! Form::label('title', 'タイトル:') !!}
+        {!! Form::text('title') !!}
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
